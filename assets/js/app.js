f49e7f93cd33960bc8b1fab71a42b461287d7530
msjtr <msjt301@gmail.com>
2026-06-13 01:45:41 +0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,72 @@
 import { Clock } from "./clock.js";
 import { DateManager } from "./date.js";
+import { Settings } from "./settings.js";
+import { Layouts } from "./layouts.js";
+import { Themes } from "./themes.js";
+import { Logos } from "./logos.js";
+import { Messages } from "./messages.js";
+import { Occasions } from "./occasions.js";
+import { Countdown } from "./countdown.js";
+import { Weather } from "./weather.js";
+import { Prayers } from "./prayers.js";
 
 document.addEventListener(
-    "DOMContentLoaded",
-    initializeApp
+"DOMContentLoaded",
+initializeApp
 );
 
 function initializeApp() {
 
-    Clock.start();
+```
+console.log(
+    "Smart Digital Clock Started"
+);
 
-    DateManager.update();
+// الوقت
+Clock.start();
 
-    console.log(
-        "Smart Digital Clock Started"
-    );
+// التاريخ
+DateManager.update();
+
+// الإعدادات
+Settings.init();
+
+// التخطيط الذكي
+Layouts.init();
+
+// الثيمات
+Themes.load();
+
+// الشعارات
+Logos.loadUniversity();
+
+if (
+    typeof Logos.loadCollege ===
+    "function"
+) {
+    Logos.loadCollege();
+}
+
+// الرسائل
+Messages.init();
+
+// المناسبات
+Occasions.init();
+
+// العد التنازلي
+Countdown.init();
+
+// مؤقت تجريبي لمدة ساعة
+Countdown.start(
+    new Date(
+        Date.now() + 3600000
+    )
+);
+
+// الطقس
+Weather.init();
+
+// أوقات الصلاة
+Prayers.init();
 
 }
