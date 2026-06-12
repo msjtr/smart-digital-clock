137ef869ab1d8e56078e1cef841af2e783d87f35
msjtr <msjt301@gmail.com>
2026-06-13 01:31:18 +0300

    Update date.js

--- assets/js/date.js
+++ assets/js/date.js
@@ -1,1 +1,69 @@
 
+export const DateManager = {
+
+    update() {
+
+        const now = new Date();
+
+        this.updateDay(now);
+
+        this.updateGregorian(now);
+
+        this.updateHijri(now);
+
+    },
+
+    updateDay(date) {
+
+        const element =
+            document.getElementById("dayName");
+
+        if (!element) return;
+
+        const days = [
+            "الأحد",
+            "الإثنين",
+            "الثلاثاء",
+            "الأربعاء",
+            "الخميس",
+            "الجمعة",
+            "السبت"
+        ];
+
+        element.textContent =
+            days[date.getDay()];
+
+    },
+
+    updateGregorian(date) {
+
+        const element =
+            document.getElementById("gregorianDate");
+
+        if (!element) return;
+
+        element.textContent =
+            date.toLocaleDateString("ar-SA");
+
+    },
+
+    updateHijri(date) {
+
+        const element =
+            document.getElementById("hijriDate");
+
+        if (!element) return;
+
+        element.textContent =
+            new Intl.DateTimeFormat(
+                "ar-SA-u-ca-islamic",
+                {
+                    day: "numeric",
+                    month: "long",
+                    year: "numeric"
+                }
+            ).format(date);
+
+    }
+
+};
