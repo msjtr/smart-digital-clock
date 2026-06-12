2e3abe4d2d27ba8275759b975e986bb69f7fe98e
msjtr <msjt301@gmail.com>
2026-06-13 01:34:27 +0300

    Add Themes module for theme management

--- assets/js/themes.js
+++ assets/js/themes.js
@@ -1,1 +1,40 @@
+export const Themes = {
 
+    current: "dark",
+
+    set(theme) {
+
+        this.current = theme;
+
+        document.body.setAttribute(
+            "data-theme",
+            theme
+        );
+
+    },
+
+    dark() {
+
+        this.set("dark");
+
+    },
+
+    light() {
+
+        this.set("light");
+
+    },
+
+    blue() {
+
+        this.set("blue");
+
+    },
+
+    green() {
+
+        this.set("green");
+
+    }
+
+};
