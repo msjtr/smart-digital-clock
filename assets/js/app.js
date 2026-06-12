f027a968ab54923c23ac2a1e5bc4df5cd3a54a27
msjtr <msjt301@gmail.com>
2026-06-13 02:50:25 +0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,77 +1,9 @@
-import { Clock } from "./clock.js";
-import { DateManager } from "./date.js";
-import { Settings } from "./settings.js";
-import { Layouts } from "./layouts.js";
-import { Themes } from "./themes.js";
-import { Logos } from "./logos.js";
-import { Messages } from "./messages.js";
-import { Occasions } from "./occasions.js";
-import { Countdown } from "./countdown.js";
-import { Weather } from "./weather.js";
-import { Prayers } from "./prayers.js";
+// assets/js/app.js
+import { initClock } from './clock.js';
+import { initDate } from './date.js';
 
-document.addEventListener(
-    "DOMContentLoaded",
-    initializeApp
-);
-
-function initializeApp() {
-
-    console.log("Smart Digital Clock Started");
-
-    try {
-
-        Clock.start();
-
-        DateManager.update();
-
-        Settings.init();
-
-        if (typeof Layouts.init === "function") {
-            Layouts.init();
-        }
-
-        if (typeof Themes.load === "function") {
-            Themes.load();
-        }
-
-        if (typeof Logos.loadUniversity === "function") {
-            Logos.loadUniversity();
-        }
-
-        if (typeof Logos.loadCollege === "function") {
-            Logos.loadCollege();
-        }
-
-        if (typeof Messages.init === "function") {
-            Messages.init();
-        }
-
-        if (typeof Occasions.init === "function") {
-            Occasions.init();
-        }
-
-        if (typeof Countdown.init === "function") {
-            Countdown.init();
-        }
-
-        if (typeof Weather.init === "function") {
-            Weather.init();
-        }
-
-        if (typeof Prayers.init === "function") {
-            Prayers.init();
-        }
-
-        console.log("Application Loaded");
-
-    } catch (error) {
-
-        console.error(
-            "Initialization Error:",
-            error
-        );
-
-    }
-
-}
+document.addEventListener('DOMContentLoaded', () => {
+    initClock();
+    initDate();
+    console.log("النظام يعمل بنجاح - تم تهيئة الوقت والتاريخ");
+});
