cf27920d64d5206c63307c33443baae8a3c416cb
msjtr <msjt301@gmail.com>
2026-06-14 03:55:14 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "hail-smart-clock-v24"; // تم تحديث الإصدار
+const CACHE_NAME = "hail-smart-clock-v25"; // تم تحديث الإصدار
 
 // تم إضافة جميع الملفات والوحدات اللازمة لتجنب كسر النظام أوفلاين
 const ASSETS_TO_CACHE = [
